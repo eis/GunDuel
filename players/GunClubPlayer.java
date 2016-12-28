89d8cad9abfc84c545d455ee855e5f118199e73e
flawr <flawr@users.noreply.github.com>
2016-12-28 15:45:37 +0100

    Create GunClubPlayer.java

---
+++ players/GunClubPlayer.java
@@ -0,0 +1,21 @@
+package players;
+
+public class GunClubPlayer extends Player {
+	
+	public  GunClubPlayer(int opponent) {
+		super(opponent);
+	}
+	public  GunClubPlayer() {
+		this(-1);
+	}
+
+	public Action fight() {
+	        if (getTurn() % 2 == 0){
+	            return load();
+	        } else {
+	            return bullet();
+	        }
+	}
+	
+
+}
