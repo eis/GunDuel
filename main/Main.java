97db2bdeb964d91074fef5dc35651a767a163e02
eis <eis@iki.fi>
2016-12-30 17:05:05 +0200

    named players approprietly

--- main/Main.java
+++ main/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	public static void main(String[] args){
 		// Total number of turns per duel.
 		int duelLength = 100;
-		// Player identifier 1: HumanPlayer.
-		Player human = new OpportunistPlayer(2);
-		// Player identifier 2: GunClubPlayer.
-		Player gunClub = new GunClubPlayer(1);
+		// Player identifier 1
+		Player player1 = new OpportunistPlayer(2);
+		// Player identifier 2
+		Player player2 = new GunClubPlayer(1);
 		
 		// Prepares a duel.
-		GunDuel duel = new GunDuel(human,gunClub, duelLength);
+		GunDuel duel = new GunDuel(player1, player2, duelLength);
 		// Start a duel.
 		duel.fight();
 		
